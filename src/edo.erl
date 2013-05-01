7070886632ccb8899674c170b9d15b13c57e3d46
Johannes Huning <mail@johanneshuning.com>
2013-05-01 14:30:12 +0200

    Use -extra and explicit halt to read cmdline args

--- src/edo.erl
+++ src/edo.erl
@@ -1,11 +1,10 @@
 -module (edo).
 -export ([start/0, start/1]).
 
--spec start() -> ok.
+-spec start() -> no_return().
 start() -> start([]).
 
--spec start([string()]) -> ok.
-start(Args0) ->
-    ArgStr = binary_to_list(base64:decode(list_to_binary(Args0))),
-    Args = string:tokens(ArgStr, " "),
-    io:format("Argument tokens: ~p~n", [Args]).
+-spec start([string()]) -> no_return().
+start(_Args0) ->
+    io:format("Arguments: ~p~n", [init:get_plain_arguments()]),
+    erlang:halt().
